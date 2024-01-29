9a349aa0315a194f4410c1eddfbbb0c66bd8f72b
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2024-01-29 22:53:32 +0300

    create serviceregistration

---
+++ service-registration/src/main/java/uz/javadev/serviceregistration/ServiceRegistrationApplication.java
@@ -0,0 +1,13 @@
+package uz.javadev.serviceregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistrationApplication.class, args);
+	}
+
+}
---
+++ service-registration/src/test/java/uz/javadev/serviceregistration/ServiceRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.javadev.serviceregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
