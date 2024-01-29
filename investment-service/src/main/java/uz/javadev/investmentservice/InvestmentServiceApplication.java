d3f9796dc8ac2aa97114e3b7f1c70e84797c1cae
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2024-01-29 22:53:08 +0300

    create investmentservice

---
+++ investment-service/src/main/java/uz/javadev/investmentservice/InvestmentServiceApplication.java
@@ -0,0 +1,13 @@
+package uz.javadev.investmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentServiceApplication.class, args);
+	}
+
+}
---
+++ investment-service/src/test/java/uz/javadev/investmentservice/InvestmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.javadev.investmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
