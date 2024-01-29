d1cd7551a32e955955afaac5d0026804ccf1ee71
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2024-01-29 22:52:51 +0300

    create goalsservice

---
+++ goals-service/src/main/java/uz/javadev/goalsservice/GoalsServiceApplication.java
@@ -0,0 +1,13 @@
+package uz.javadev.goalsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalsServiceApplication.class, args);
+	}
+
+}
---
+++ goals-service/src/test/java/uz/javadev/goalsservice/GoalsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.javadev.goalsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
