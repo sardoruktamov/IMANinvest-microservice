326c6ed8d369583041e76c761922d15807f09e8d
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2024-01-29 22:51:59 +0300

    create cloudconfiguration

---
+++ cloud-configuration/src/main/java/uz/javadev/cloudconfiguration/CloudConfigurationApplication.java
@@ -0,0 +1,13 @@
+package uz.javadev.cloudconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigurationApplication.class, args);
+	}
+
+}
---
+++ cloud-configuration/src/test/java/uz/javadev/cloudconfiguration/CloudConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.javadev.cloudconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
